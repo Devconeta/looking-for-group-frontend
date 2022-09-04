31b2768a4a9ffbae989320564783a2addf4b4541
nvcho <ignacio_0701@hotmail.com>
2022-09-04 19:08:21 -0300

    [ADD] _document.js and fonts

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
@@ -12,9 +13,23 @@ export default function Home() {
       </Head>
 
       <main className="mainContainer">
-        <header className="flex flex-row justify-end p-4">
-          <ConnectButton />
-        </header>
+        <navbar className="flex flex-row justify-center items-center p-4">
+          <ul className="flex flex-row gap-20 text-3xl z-10">
+            <li>
+              <Link href="/">Getting started</Link>
+            </li>
+            <li>
+              <Link href="/events">Hackathons</Link>
+            </li>
+            <li>
+              <Link href="/showcase">Showcase</Link>
+            </li>
+          </ul>
+
+          <div className="flex absolute justify-end w-full p-4 z-0">
+            <ConnectButton />
+          </div>
+        </navbar>
       </main>
     </>
   );
