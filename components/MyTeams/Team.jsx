9873b022ef9a325bf99c635b469a1d45d840107c
nvcho <cu.ouza@gmail.com>
2022-09-25 12:20:37 -0300

    [UPDATE] card team

--- components/MyTeams/Team.jsx
+++ components/MyTeams/Team.jsx
@@ -1,26 +1,31 @@
-import React, { useState } from "react";
-import TreasuryMode from "./TreasuryMode/TreasuryMode";
-import TeamMode from "./TeamMode/TeamMode";
+import React, { useState } from 'react'
+import TreasuryMode from './TreasuryMode/TreasuryMode'
+import TeamMode from './TeamMode/TeamMode'
 
 const Team = ({ team, setEditMode }) => {
-  const [treasuryMode, setTreasuryMode] = useState(false);
+	const [treasuryMode, setTreasuryMode] = useState(false)
 
-  return (
-    // TEAM CONTAINER
-    <div className="flex min-h-[26rem] rounded-xl border-[1px] border-white/30 bg-black/30 p-8 text-primaryLight backdrop-blur-lg">
-      {/* TEAM  MODE  */}
-      {!treasuryMode ? (
-        <TeamMode
-          team={team}
-          setTreasuryMode={setTreasuryMode}
-          setEditMode={setEditMode}
-        />
-      ) : (
-        // TREASURY MODE
-        <TreasuryMode team={team} setTreasuryMode={setTreasuryMode} />
-      )}
-    </div>
-  );
-};
+	return (
+		// TEAM CONTAINER
+		<div className="flex min-h-[26rem] h-full w-full rounded-xl border-[1px] border-white/30 bg-black/30 p-8 text-primaryLight backdrop-blur-lg">
+			{/* TEAM  MODE  */}
+			<div>
+				{!treasuryMode ? (
+					<TeamMode
+						team={team}
+						setTreasuryMode={setTreasuryMode}
+						setEditMode={setEditMode}
+					/>
+				) : (
+					// TREASURY MODE
+					<TreasuryMode
+						team={team}
+						setTreasuryMode={setTreasuryMode}
+					/>
+				)}
+			</div>
+		</div>
+	)
+}
 
-export default Team;
+export default Team
